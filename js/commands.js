e93198d944ce3d7a3240f7a0aab381d9f7377c9a
Garvit <122175234+garvit-exe@users.noreply.github.com>
2024-03-28 20:01:09 +0530

    Deployed terminal portfolio

---
+++ js/commands.js
@@ -0,0 +1,121 @@
+var password = "garvit";
+var linkedin = "https://www.linkedin.com/in/garvit-budhiraja/";
+var instagram = "https://www.instagram.com/garvit__budhiraja/";
+var github = "https://github.com/garvit-exe/";
+var vit = "https://vitbhopal.ac.in/";
+var ipcs = "http://www.ipcseducation.com/";
+var dps = "https://www.dpsrohtak.org/";
+
+about = [
+  "<br>",
+  "Hi, My name is Garvit",
+  "I am an aspiring software engineer who builds engaging websites like this one.",
+  "<br>",
+  "I am working on my bachelor's degree at Vellore Institute of Technology, Bhopal",
+  "where I am working on my skills to innovate something no one has ever imagined.",
+  "Despite saying that, it is something even I have not imagined yet, so bear with me.",
+  "<br>",
+  "Recently, I have taken a huge interest in problem-solving, iOS development,",
+  "web development, capture the flag as well as machine learning, and therefore,",
+  "you will be seeing a lot of cool projects from me in the future.",
+  "",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/garvit-budhiraja' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/garvit__budhiraja' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/garvit-exe' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">help -l</span>        Want to see more commands?',
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  '<span class="command">edu-secret</span>     Use to know a secret about my education',
+  '<span class="command">ilovemyfamily</span>  Do you love your family?',
+  '<span class="command">itchynavel</span>     Want to see something cool?',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still developing... most projects are either offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+education = [
+  "<br>",
+  '<span class="command">VIT</span>            Vellore Institute of Technology',
+  '<span class="command">IPCS</span>           Indraprastha Convent Senior Secondary School',
+  '<span class="command">DPS</span>            Delhi Public School',
+  "<br>",
+];
+
+educationLong = [
+  "<br>",
+  '<span class="command">VIT</span>            Vellore Institute of Technology, Bhopal',
+  'B.Tech. program - Computer Science - University is meh.',
+  "<br>",
+  '<span class="command">IPCS</span>           Indraprastha Convent Senior Secondary School, Rohini',
+  'Completed class 11 and 12 from this school.',
+  "<br>",
+  '<span class="command">DPS</span>            Delhi Public School, Rohtak',
+  'Spent my majority of life here till I changed schools after class 10.',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          Want to stalk me?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">edu</span>            Know my degrees',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+helpLong = [
+  "<br>",
+  '<span class="command">about</span>          Want to stalk me?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">instagram</span>      Want my insta?',
+  '<span class="command">linkedin</span>       Oh professional, aren\'t you?',
+  '<span class="command">github</span>         See cool projects like this',
+  '<span class="command">edu</span>            Know my degrees',
+  '<span class="command">edu -l</span>         Know my degrees in detail',
+  '<span class="command">vit</span>            How is my university?',
+  '<span class="command">ipcs</span>           What about my school?',
+  '<span class="command">dps</span>            My favourite institute of all time',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">password</span>       Here is the password',
+  '<span class="command">**hidden**</span>     Hidden Commands - Need access',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">help -l</span>        You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+itchynavel = [
+  '<span class="index">Budhiraja Industries (Garvit Budhiraja) Not A Corporation. All knights reserved.</span>',
+  " ,_____                  _ _                   ",
+  " / ___|  __ _ _ ____   _(_) |_                 ",
+  "| |  _ / _` | '__\\ \\ / / | __|                ",
+  "| |_| | (_| | |   \\ V /| | |_                 ",
+  " \\_____|\\__,_|_|  __\\_/ |_|\\__|        _       ",
+  " | __ ) _   _  __| | |__ (_)_ __ __ _ (_) __ _ ",
+  " |  _ \\| | | |/ _` | '_ \\| | '__/ _` || |/ _` |",
+  " | |_) | |_| | (_| | | | | | | | (_| || | (_| |",
+  " |____/ \\__,_|\_,_|_|_| |_|_|_|  \\__,_|| |\\__,_|",
+  "                                    |__/",
+  '<br>',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<br>'
+];
---
+++ js/main.js
@@ -0,0 +1,191 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(itchynavel, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("(base) visitor@Garvits-Portfolio:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "help -l":
+      loopLines(helpLong, "color2 margin", 80);
+      break;
+    case "about":
+      loopLines(about, "color2 margin", 80);
+      break;
+    case "sudo":
+      addLine("Oh no, you're not admin...", "color2", 80);
+      setTimeout(function() {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+      }, 1000); 
+      break;
+    case "edu-secret":
+      addLine("I started studying in DPS from prep. I was in Scholars Rosary till nursery.", "color2", 80);
+      break;
+    case "ilovemyfamily":
+      addLine("Aww... I love my family as well.", "color2", 80);
+      break;
+    case "itchynavel":
+      loopLines(itchynavel, "color2 margin", 80);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "secret":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "edu":
+      loopLines(education, "color2 margin", 80);
+      break;
+    case "edu -l":
+      loopLines(educationLong, "color2 margin", 80);
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "password":
+      addLine("<span class=\"inherit\">Try harder than that!</span>", "error", 100);
+      console.log(password);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    case "vit":
+      addLine("Opening VIT Bhopal's Website...", "color2", 0);
+      newTab(vit);
+      break;
+    case "ipcs":
+      addLine("Opening IPCS Rohini's Website...", "color2", 0);
+      newTab(ipcs);
+      break;
+    case "dps":
+      addLine("Opening DPS Rohtak's Website...", "color2", 0);
+      newTab(dps);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
